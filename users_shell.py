7a53c2caf8c1558616bcd039308b02686059189c
jose <josejm@jose-hab-eos>
2015-01-27 19:16:52 +0100

    Ejercicio 13.4 con num de usuarios

--- users_shell.py
+++ users_shell.py
@@ -4,11 +4,9 @@ fich=open("/etc/passwd","r")
 users_lines = fich.readlines()
 fich.close()
 
-dicc = {}
-
 for user_line in users_lines:
     lista_user = user_line.split(":")
-    user = lista_user[0]
-    shell = lista_user[-1][:-1] #Principio a final menos el /n
-    dicc [username] = shell
-    print user +" su shell es "+ shell,
+    username = lista_user[0]
+    shell = lista_user[-1][:-1]
+    print username +" --> "+ shell
+print "hay", len(users_lines), "usuarios"
